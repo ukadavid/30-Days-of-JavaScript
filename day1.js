3300c529e727b59d18d15525d0460bfda840de8e
ukadavid <ukadavid7@gmail.com>
2022-06-21 12:16:44 +0100

    day1 files

---
+++ day1.js
@@ -0,0 +1,41 @@
+//Solutions to Day 1
+// Welcome to 30DaysOfJavaScript **This is the solution to number 1**
+/*  comments can make code readable, easy to reuse and informative   ***This is the solution to number 2   */
+/*  To create a js file can be created on your VScode or on your file manager, just make sure it has a ".js" at the end of the name of the file  ***This is the solution to number 3   */
+// ***This is the solution to number 4
+let stringName = 'hiya';
+let booleanValue = true;
+let undefinedValue = undefined;
+let nullValue = null;
+
+/*  create the datatypes folder and add ".js" at the end of the name of the file  ***This is the solution to number 5   */
+
+// ***This is the solution to number 6
+console.log(typeof(stringName));
+console.log(typeof(booleanValue));
+console.log(typeof(undefinedValue));
+console.log(typeof(nullValue));
+
+// ***This is the solution to number 7
+let hiya = 'Please learn coding';
+let fish = 'salmon';
+let num = 1000000000n;
+let numValue = 34;
+
+// ***This is the solution to number 8
+let firstName = 'Tobechukwu';
+let lastName = 'Uka';
+let maritalStatus = 'Single';
+let country = 'Nigeria';
+let age = 20 //this is not my real age lol
+
+// ***This is the solution to number 9
+firstName, lastName, maritalStatus, country, age = 'Tobechukwu', 'Uka', 'Single', 'Nigeria', 20;
+
+// ***This is the solution to number 9
+let myAge = 25;
+let yourAge = 30;
+let ageWord = 'years old'
+console.log(`I am ${myAge} ${ageWord}\nYou are ${yourAge} ${ageWord}`)
+
+// 🎉 CONGRATULATIONS ! 🎉
