192e4c2e1e797778a0b0bc4df2d6db08bd21d428
ukadavid <ukadavid7@gmail.com>
2022-06-21 12:29:56 +0100

    arrangement of the folders

---
+++ Day 2/index2.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>30 days of JS</title>
+  </head>
+  <body>
+    <h1>30 days of JS</h1>
+    <p>Day 2</p>
+    <script src="./day2.js"></script>
+  </body>
+</html>
